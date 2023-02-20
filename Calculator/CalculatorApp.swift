b714151373604b708e5c8a63fd3f582ddfc686d4
MisakaTang <1010565305@qq.com>
2023-02-20 15:44:13 +0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 唐伟业 on 2023/2/20.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
